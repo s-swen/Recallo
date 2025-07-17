a078e3cdf5daf1891600bc86e79ec2f892b4ffde
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-07-17 12:12:17 +0000

    added StudyItem

--- recallo/recallo/settings.py
+++ recallo/recallo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# added
+
+AUTH_USER_MODEL = 'api.User'
